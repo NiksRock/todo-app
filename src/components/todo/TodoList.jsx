e27e3e120dc255a355d509e6a23a6123e07c1b3c
Nikunj Patel <nikunjpatel1581996@gmail.com>
2023-02-21 12:15:43 +0530

    unused react remove

--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,4 +1,4 @@
-import react, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import db from "./../../firebase";
 const TodoList = (props) => {
   var [todoList, setTodoList] = useState({});
